83b361754ff6a93792db2542752edcfd292fbfd8
Hafiz Rahim Adnan <hafizrahim3011@gmail.com>
2025-05-13 00:57:25 +0500

    feat: implement JWT verification middleware for user authentication

---
+++ server/src/middlewares/auth.middelware.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import { wrapAsync } from "../utils/wrapAsync.js";
+import { ApiError } from "../utils/ApiError.js";
+
+const verifyJwt = (role) =>
+  wrapAsync(async (req, res, next) => {
+    try {
+      const tokens =
+        req.cookies.accessToken ||
+        req.header("Authorization")?.replace("Bearer", "");
+      if (!tokens) {
+        throw new ApiError(403, "User is unauthorized");
+      }
+      const decodedToken = jwt.verify(tokens, process.env.ACCESS_TOKEN_KEY);
+      if (!decodedToken._id) {
+        throw new ApiError(
+          500,
+          "Somethign went wrong while Decoding the tokens"
+        );
+      }
+
+      const user = await User.findById(decodedToken._id).select(" -passowrd");
+
+      if (!user) {
+        throw new ApiError(500, "Failed to find the User");
+      }
+      if (role && user.role !== role) {
+        throw new ApiError(403, `Access Denied to the ${role}`);
+      }
+
+      req.user = user;
+      next();
+    } catch (error) {
+      throw new ApiError(error.statusCode, error.message);
+    }
+  });
+
+export { verifyJwt };
