ee112897cbbdcc1bd972f8344e45cc502724d620
Hafiz Rahim Adnan <hafizrahim3011@gmail.com>
2025-06-10 15:08:11 +0500

    fix vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: ProcessingInstruction.env.VITE_BASE_PATH || "/FMS",
+  base: process.env.VITE_BASE_PATH || "/FMS",
 });
