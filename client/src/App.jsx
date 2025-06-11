4e3b40b86c8fc4d5f453cf3711240e96a9e61124
Hafiz Rahim Adnan <hafizrahim3011@gmail.com>
2025-06-11 22:03:29 +0500

    update App component title and add react-router-dom dependency

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import viteLogo from "/vite.svg";
 function App() {
   return (
     <>
-      <h1 style={{ color: "red" }}>Hello World This is my vercel Rect App</h1>
+      <h1>FMS (flock management System)</h1>
     </>
   );
 }
