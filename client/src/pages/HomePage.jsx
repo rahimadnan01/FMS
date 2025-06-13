6e4127c86bcd88674f38ea861c96c2f75d647d09
Hafiz Rahim Adnan <hafizrahim3011@gmail.com>
2025-06-13 23:43:12 +0500

    fix: enhance responsive design for login and register pages with media queries

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,12 +3,6 @@ function HomePage() {
   return (
     <>
       <h1>Hello I am Home Page</h1>
-      <li className="login-link">
-        <NavLink to="/FMS/register">Do not have an account? Signup</NavLink>
-      </li>
-      <li className="login-link">
-        <NavLink to="/FMS/login">login page</NavLink>
-      </li>
     </>
   );
 }
