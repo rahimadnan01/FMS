41caabcf1741e3eab6dfaaaf1316fdec099b2220
Hafiz Rahim Adnan <hafizrahim3011@gmail.com>
2025-06-13 22:57:52 +0500

    fix: update base path in vite.config.js and ensure correct deployment settings in package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/FMS",
+  base: "/FMS", // 👈 IMPORTANT
 });
